6a521e26e3f9034128c1349b04d2c0467536fa21
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2019-11-27 14:13:21 -0500

    Erratum #1

--- MusicStore/Models/Depot.cs
+++ MusicStore/Models/Depot.cs
@@ -1,5 +1,5 @@
 ﻿namespace MusicStore.Models {
-
+    using MusicStore.Models.DAL;
     public class Depot {
         public DALUtilisateur Utilisateurs { get; private set; } = new DALUtilisateur();
     }
